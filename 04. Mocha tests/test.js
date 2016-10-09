f842d5eb36800af4126b1b712165f0f02041f165
4m1g0 <dev.4m1g0@gmail.com>
2016-10-09 21:05:33 +0200

    mocha tests

---
+++ 04. Mocha tests/test.js
@@ -0,0 +1,22 @@
+// ./node_modules/mocha/bin/mocha -R nyan test.js
+var assert = require('assert');
+
+describe('my feature', function() {
+    it('works', function() {
+        assert.equal('A', 'A');
+    });
+
+    it('fails gracefully', function() {
+        assert.throws(function() {
+            throw 'Error';
+        });
+    });
+});
+
+describe('my other feature', function() {
+    it('async', function(done) {
+        setTimeout(function() {
+            done();
+        }, 25);
+    });
+});
