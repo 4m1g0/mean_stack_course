bf5417d689d87f40b830649a156de983d1113eaf
4m1g0 <dev.4m1g0@gmail.com>
2017-07-16 22:34:57 +0200

    Add navbar logic

--- 08. Angular/karma.local.conf.js
+++ 08. Angular/karma.local.conf.js
@@ -1,17 +1,19 @@
 module.exports = function(config) {
-    config.set({
-        files: [
-            'http://code.jquery.com/jquery-1.11.3.js',
-            'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular.js',
-            // ngMockE2E
-            'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular-mocks.js',
-            './app.js',
-            './tests.js'
-        ],
-        frameworks: ['mocha', 'chai'],
-        browsers: ['Chrome'],
-        proxies: {
-            '/': 'http://localhost:3000'
-        }
-    });
+  config.set({
+    files: [
+      'http://code.jquery.com/jquery-1.11.3.js',
+      'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular.js',
+      // For ngMockE2E
+      'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular-mocks.js',
+      './bin/index.js',
+      './test.js',
+      { pattern: './templates/*.html', included: false, served: true }
+    ],
+    frameworks: ['mocha', 'chai'],
+    browsers: ['Chrome'],
+    port: 9876,
+    proxies : {
+      '/': 'http://localhost:9876/base/'
+    }
+  });
 };
